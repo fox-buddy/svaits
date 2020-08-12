e82511ba570a179f3ebc63688ff962ee14f918d6
fox-buddy <48259046+fox-buddy@users.noreply.github.com>
2020-08-12 22:36:26 +0200

    Definition Input Values

--- src/app/core/IStockData.ts
+++ src/app/core/IStockData.ts
@@ -1,10 +1,64 @@
 export interface IStockResults {
-  fairValue: Number;
-  eigenkapitalquote: Number;
+  fairValue: number;
+  eigenkapitalquote: number;
 }
 
 export interface IStockInputs {
+  bilanzSummeInMillionenZumStichtag: number;
+  bilanzSummeDescription: string;
 
+  eigenKapitalInMillionenZumStichtag: number;
+  eigenKapitalDescription: string;
+
+  eigenKapitalThreeYearAverageInMillionen: number;
+  eigenKapitalThreeYearAverageDescription: string;
+
+  gesamtVerbindlichKeitenInMillionenZumStichtag: number;
+  gesamtVerbindlichKeitenDescription: string;
+
+  zahlungsMittelInMillionenZumStichtag: number;
+  zahlungsMittelDescription: string;
+
+  operativerCashflowInMillionenZumStichtag: number;
+  operativerCashflowDescription: string;
+
+  operativerCashflowThreeYearAverageInMillionen: number;
+  operativerCashflowThreeYearAverageDescription: string;
+
+  investmentCashflowInMillionenZumStichtag: number;
+  investmentCashflowDescription: string;
+
+  investmentCashflowThreeYearAverageInMillionen: number;
+  investmentCashflowThreeYearAverageDescription: string;
+
+  umsatzInMillionenZumStichtag: number;
+  umsatzInMillionenDescription: string;
+
+  ebitInMillionenZumStichtag: number;
+  ebitDescription: string;
+
+  eatInMillionenZumStichtag: number;
+  eatDescription: string;
+
+  marktKapitalisierungInMillionenZumStichtag: number;
+  marktKapitalisierungDescription: string;
+
+  umsatzChangeFirstPeriod: number;
+  umsatzChangeSecondPeriod: number;
+  umsatzChangeThirdPeriod: number;
+  umsatzChangeFourthPeriod: number;
+  umsatzChangeFifthPeriod: number;
+  umsatzChangeSixthPeriod: number;
+  umsatzChangeDescription: string;
+
+  expectedRateOfReturnPercent: number;
+  expectedRateOfReturnDescription: string;
+
+  expectedLongGrowRatePercent: number;
+  expectedLongGrowRateDescription: string;
+
+  securityMarginRate: number;
+  securityMarginDescription: string;
 }
 
 export interface IStock {
--- src/app/core/stock.service.ts
+++ src/app/core/stock.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { IStock, IStockResults } from './IStockData'
+import { IStock, IStockResults, IStockInputs } from './IStockData'
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,7 @@ export class StockService {
 
   public addNewStockEntry(keyName: string) {
     this.stockCollection.push({
-      stockName: keyName, inputData: {}, resultData: this.resultDataEmpty()
+      stockName: keyName, inputData: this.inputDataEmpty(), resultData: this.resultDataEmpty()
     });
 
 
@@ -27,8 +27,52 @@ export class StockService {
 
   }
 
-  private inputDataEmpty() {
+  private inputDataEmpty(): IStockInputs {
+    return {
+      bilanzSummeInMillionenZumStichtag: 0,
+      bilanzSummeDescription: 'Eigenkapital + Fremdkapital zum letzten Bilanzstichtag',
+      eigenKapitalInMillionenZumStichtag: 0,
+      eigenKapitalDescription: 'gesamtes Eigenkapital zum letzten Bilanzstichtag',
+      eigenKapitalThreeYearAverageInMillionen: 0,
+      eigenKapitalThreeYearAverageDescription: 'gemitteltes Eigenkapital der letzten 3 Bilanzstichtage',
+      gesamtVerbindlichKeitenInMillionenZumStichtag: 0,
+      gesamtVerbindlichKeitenDescription: 'gesamtes Fremdkapital zum letzten Bilanzstichtag',
+      zahlungsMittelInMillionenZumStichtag: 0,
+      zahlungsMittelDescription: 'Summe der Zahlungsmittel zum letzten Bilanzstichtag',
+      operativerCashflowInMillionenZumStichtag: 0,
+      operativerCashflowDescription: 'operativer Cashflow zum letzten Bilanzstichtag',
+      operativerCashflowThreeYearAverageInMillionen: 0,
+      operativerCashflowThreeYearAverageDescription: 'gemittelter operativer Cashflow der letzten 3 Bilanzstichtage',
+      investmentCashflowInMillionenZumStichtag: 0,
+      investmentCashflowDescription: 'cashflow aus Investitionstätigkeit zum letzten Bilanzstichtag',
+      investmentCashflowThreeYearAverageInMillionen: 0,
+      investmentCashflowThreeYearAverageDescription: 'gemittelter cashflow aus Investitionstätigkeit der letzten 3 Bilanzstichtage',
+      umsatzInMillionenZumStichtag: 0,
+      umsatzInMillionenDescription: 'Umsatzergebnis zum letzten Bilanzstichtag',
+      ebitInMillionenZumStichtag: 0,
+      ebitDescription: 'Erlöse vor Steuern und Zinsen',
+      eatInMillionenZumStichtag: 0,
+      eatDescription: 'Erlöse nach Steuern. In Deutschland Jahresendergebnis',
+      marktKapitalisierungInMillionenZumStichtag: 0,
+      marktKapitalisierungDescription: 'Maktkapitalisierung. Gesamter Börsenwert',
 
+      umsatzChangeFirstPeriod: 0,
+      umsatzChangeSecondPeriod: 0,
+      umsatzChangeThirdPeriod: 0,
+      umsatzChangeFourthPeriod: 0,
+      umsatzChangeFifthPeriod: 0,
+      umsatzChangeSixthPeriod: 0,
+      umsatzChangeDescription: 'Umsatzveränderungen der letzten 6 Jahre zum Stichtag im Vergleich zum Vorjahr',
+
+      expectedRateOfReturnPercent: 0,
+      expectedRateOfReturnDescription: 'Erwartete Rendite. Sollte mindestens 6 Prozent betragen (Immobiliendurchschnitt)',
+
+      expectedLongGrowRatePercent: 0,
+      expectedLongGrowRateDescription: 'Erwartetes Langzeitwachstum. Sollte konservativ gewählt werden, da die Zukunft schwer abschätzbar ist. Eine Rate im Bereich der Inflation beispielsweise (3)',
+
+      securityMarginRate: 0,
+      securityMarginDescription: 'Sicherheitsmarge zum Abwerten des errechneten Wertes. 5 bis 10 Prozent beispielsweise'
+    }
   }
 
   private resultDataEmpty(): IStockResults {
